88ecf52a3a7efef7d4df1489868b558e1d79b6b2
Yadiel <ycabr035@fiu.edu>
2018-06-11 20:51:25 -0400

    added helper function

--- script.js
+++ script.js
@@ -7,8 +7,14 @@ const theTimer = document.querySelector(".timer");
 var timer = [0,0,0,0];
 
 // Add leading zero to numbers 9 or below (purely for aesthetics):
+function leadingZero(time) {
+  if (time <= 0){
+    time = "0" + time;
 
+  }
+  return time; 
 
+}
 // Run a standard minute/second/hundredths timer:
 function runTimer() {
   let currentTime = timer[0] + ":" + timer[1] + ":" + timer[2];
@@ -17,7 +23,8 @@ function runTimer() {
 
   timer[0] = Math.floor((timer[3]/100)/60);
   timer[1] = Math.floor((timer[3]/100) - (timer[0] * 60));
-  timer[2] = Math.floor(timer[3] - (timer[1] * 100) - (time[0] * 6000)); 
+  timer[2] = Math.floor(timer[3] - (timer[1] * 100) - (time[0] * 6000));
+
 }
 
 // Match the text entered with the provided text on the page:
