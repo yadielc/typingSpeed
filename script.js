d29184dbaae7f904e86829e7bb4d0814cc6e6f1e
Yadiel <ycabr035@fiu.edu>
2018-06-11 20:49:18 -0400

    events listeners to detect typing

--- script.js
+++ script.js
@@ -14,6 +14,10 @@ function runTimer() {
   let currentTime = timer[0] + ":" + timer[1] + ":" + timer[2];
   theTimer.innerHTML = currentTime;
   timer[3]++;
+
+  timer[0] = Math.floor((timer[3]/100)/60);
+  timer[1] = Math.floor((timer[3]/100) - (timer[0] * 60));
+  timer[2] = Math.floor(timer[3] - (timer[1] * 100) - (time[0] * 6000)); 
 }
 
 // Match the text entered with the provided text on the page:
